67beac7812db586c0c7c8b8f3bd9a5a3aae8bc28
Gunjlkham08 <gunjegunje54@gmail.com>
2024-12-02 17:55:10 +0800

    added a little thing

--- src/app/page.js
+++ src/app/page.js
@@ -1,150 +1,65 @@
 
-function TodoList() {
-  const [tasks, setTasks] = useState([
-  {
-  id: 1,
-  text: 'Doctor Appointment',
-  completed: true
-  },
-  {
-  id: 2,
-  text: 'Meeting at School',
-  completed: false
-  }
-  ]);
-  
-  const [text, setText] = useState('');
- function addTask(text) {
-  const newTask = {
-  id: Date.now(),
-  text,
-  completed: false
-  };
-  setTasks([tasks, newTask]);
-  setText('');
-  }
- function deleteTask(id) {
-  setTasks(tasks.filter(task => task.id !== id));
-  }
- function toggleCompleted(id) {
-  setTasks(tasks.map(task => {
-  if (task.id === id) {
-  return {task, completed: !task.completed};
-  } else {
-  return task;
-  } 
-  }));
-  }
- return (
-  <div className="todo-list">
-  {tasks.map(task => (
-  <TodoItem
-  key={task.id} 
-  task={task}
-  deleteTask={deleteTask}
-  toggleCompleted={toggleCompleted} 
-  />
-  ))}
- <input
-  value={text}
-  onChange={e => setText(e.target.value)} 
-  />
- <button onClick={() => addTask(text)}>Add</button>
-  </div>
-  );
- }
- export default TodoList;
- function TodoItem({ task, deleteTask, toggleCompleted }) {
- function handleChange() {
-  toggleCompleted(task.id);
-  }
-  
-  return (
-  <div className="todo-item">
-  <input 
-  type="checkbox"
-  checked={task.completed}
-  onChange={handleChange}
-  />
- <p>{task.text}</p>
- <button onClick={() => deleteTask(task.id)}>
-  X
-  </button>
-  </div>
-  );
- }
 
 "use client";
 
 import { useState } from "react";
 import "./globals.css";
 
-// export default function Tasks() {
-//   const [tasks, setTasks] = useState([{ id: 1, task: "cleaning", status: "to-do" }]);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-//   const [taskName, setTaskName] = useState("");
-//   const openModal = () => setIsModalOpen(true);
-//   const closeModal = () => setIsModalOpen(false);
-//   const addTask = () => {
-//     if (taskName.trim()) {
-//       const newTask = { id: tasks.length + 1, task: taskName, status: "to-do" };
-//       setTasks([...tasks, newTask]);
-//       setTaskName(""); 
-//       closeModal(); 
-//     } else {
-//       alert("Please enter a task name!");
-//     }
+const data=[]
+async function fetchData(){
+  const response = await fetch ('https://api.thecatapi.com/v1/images/0XYvRd7oD')
+  const data = await response.json();
+  console.log(data);
+  return(data);
+}
+
+// export default function Page() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const openModal = () => {
+//     setIsOpen(true);
 //   };
-
+//   const closeModal = () => {
+//     setIsOpen(false);
+//   };
 //   return (
-//     <>
-//       <button onClick={openModal} className="add-task-btn">
-//         Add Task
-//       </button>
-//       {isModalOpen && (
-//         <Modal 
-//           taskName={taskName} 
-//           setTaskName={setTaskName} 
-//           addTask={addTask} 
-//           closeModal={closeModal} 
-//         />
-//       )}
-//       <div className="task-container">
-//         {tasks.map((task) => (
-//           <div key={task.id} className="task">
-//             <p>{task.task}</p>
-//             <span>{task.status}</span>
+//     <div>
+//       <button onClick={openModal}>add</button>
+//       {isOpen && (
+//         <div
+//           style={{
+//             height: "100vh",
+//             backgroundColor: "rgba(0,0,0,0.5)",
+//             alignContent: "center",
+//           }}
+//         >
+//           <div
+//             style={{
+//               height: 500,
+//               width: 800,
+//               backgroundColor: "white",
+//               borderRadius: 16,
+//               margin: "auto",
+//               padding: 48,
+//             }}
+//             >
+//             <button onClick={closeModal}>close</button>
+//             <input
+//               style={{
+//                 width: 256,
+//                 height: 56,
+//                 border: "1px solid black",
+//                 padding: 4,
+//               }}
+//               placeholder="task"
+//             ></input>
 //           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// }
-
-// export function Modal({ taskName, setTaskName, addTask, closeModal }) {
-//   return (
-//     <div id="modal" className="modal-open">
-//       <div className="modal-content">
-//         <h1 className="title2">Enter Task</h1>
-//         <div className="modal-top">
-//           <input
-//             type="text"
-//             id="task-name"
-//             placeholder="Task name..."
-//             value={taskName}
-//             onChange={(e) => setTaskName(e.target.value)} 
-//           />
-//           <button onClick={addTask} className="submit">
-//             Submit
-//           </button>
-//           <button onClick={closeModal} className="cancel">
-//             Cancel
-//           </button>
 //         </div>
-//       </div>
+//       )}
 //     </div>
 //   );
-// }
+// } 
+
 
 // +-
 // export default function Numbers() {
